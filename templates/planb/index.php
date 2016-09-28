c1b86ee3a28052397c6efbeb43f478260e33432d
Joan <joanpiferrer@gmail.com>
2016-09-28 23:44:41 +0200

    afegir assets

--- templates/planb/index.php
+++ templates/planb/index.php
@@ -78,15 +78,9 @@ if ($this->params->get('logoFile')) {
     }
     ?>
     <!-- Bootstrap -->
-    <link href="templates/planb/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="templates/planb/css/bootstrap/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
-    <link href="templates/planb/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <!-- iCheck -->
-    <link href="templates/planb/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
-    <!-- bootstrap-progressbar -->
-    <link href="templates/planb/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css"
-          rel="stylesheet">
-
+    <link href="templates/planb/css/font-awesome/font-awesome.min.css" rel="stylesheet">
     <!-- Custom Theme Style -->
     <link href="templates/planb/css/custom.css" rel="stylesheet">
 </head>
